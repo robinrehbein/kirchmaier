0e58e1d276d00bd49a258365333a6777c3440043
Andi Lemberger <andi.lemberger@gmx.de>
2021-05-15 14:38:06 +0200

    fixed some errors

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Navbar from "../lib/Navbar/index.svelte";
+
+</script>
+
+<svelte:head>
+  <meta name="robots" content="noindex" />
+</svelte:head>
+
+<Navbar />
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>Made with ❤ by programr</p>
+</footer>
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,29 @@
 <script lang="ts">
-	let a = 1;
-    let b = 2;
+  let a = 1;
+  let b = 2;
 
+  // export async function load({ page, fetch, session, context }) {
+  // 	const url = `/blog/${page.params.slug}.json`;
+  // 	const res = await fetch(url);
 
+  // 	if (res.ok) {
+  // 		return {
+  // 			props: {
+  // 				article: await res.json()
+  // 			}
+  // 		};
+  // 	}
 
-	export async function load({ page, fetch, session, context }) {
-		const url = `/blog/${page.params.slug}.json`;
-		const res = await fetch(url);
+  // 	return {
+  // 		status: res.status,
+  // 		error: new Error(`Could not load ${url}`)
+  // 	};
+  // }
 
-		if (res.ok) {
-			return {
-				props: {
-					article: await res.json()
-				}
-			};
-		}
-
-		return {
-			status: res.status,
-			error: new Error(`Could not load ${url}`)
-		};
-	}
 </script>
 
-
-
-<input type="number" bind:value={a}>
-<input type="number" bind:value={b}>
+<input type="number" bind:value={a} />
+<input type="number" bind:value={b} />
 
 <p>{a} + {b} = {a + b}</p>
 
