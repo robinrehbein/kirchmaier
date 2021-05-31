0ffb6e32508e55425d909f70b8d35092fe0bd51d
Robin Rehbein <me@robinrehbein.de>
2021-05-31 19:45:45 +0200

    added stuff to footer

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -1,7 +1,41 @@
 <script lang="ts"></script>
 
 <footer>
-    <p>Made with ❤ by programr</p>
+    <ul>
+        <!-- open in modal -->
+        <li><a href="">Imprint</a></li>
+        <li><a href="">Data Protection</a></li>
+        <li><a href="">Disclaimer</a></li>
+    </ul>
+
+    <p>
+        Made with <svg
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="80px"
+            height="60px"
+            viewBox="0 0 80 60"
+            enable-background="new 0 0 80 60"
+            xml:space="preserve"
+        >
+            <polygon
+                fill="#CD1B38"
+                points="20.526,0.45 1,19.977 40.053,59.028 79.105,19.977 59.578,0.45 40.053,19.977 "
+            />
+            <polygon
+                fill="#8F1329"
+                points="40.053,20 59.578,39.526 79.105,20 "
+            />
+        </svg>
+        by <a href="https://programr.de/">programr</a>
+    </p>
+    <p id="rr_copyright">
+        Copyright © <em>2021 programr</em> - All rights reserved.
+    </p>
 </footer>
 
 <style lang="scss"></style>
