caa3c95481bb8726818269e44546ca3edcfaa0c5
Robin Rehbein <me@robinrehbein.de>
2021-09-09 13:22:04 +0200

    remove content wrapper and move to _layout.svelte

--- src/lib/util/Flex/index.svelte
+++ src/lib/util/Flex/index.svelte
@@ -1,111 +1,111 @@
 <script type="ts">
-  /* flexDirection - extend list if needed; row is default. */
-  export let flexDirectionColumn = false;
-  /* flexDirection responsiveness, generally based on switching at 1000px width */
-  export let flexDirectionResponsiveColumnToRow = false;
-  export let flexDirectionResponsiveColumnToRowReverse = false;
-  export let flexDirectionResponsiveRowToColumn = false;
-  export let flexDirectionResponsiveRowToColumnReverse = false;
+    /* flexDirection - extend list if needed; row is default. */
+    export let flexDirectionColumn = false;
+    /* flexDirection responsiveness, generally based on switching at 1000px width */
+    export let flexDirectionResponsiveColumnToRow = false;
+    export let flexDirectionResponsiveColumnToRowReverse = false;
+    export let flexDirectionResponsiveRowToColumn = false;
+    export let flexDirectionResponsiveRowToColumnReverse = false;
 
-  /* justifyContent - extend list if needed */
-  export let justifyContentSpaceBetween = false;
-  export let justifyContentSpaceAround = false;
-  export let justifyContentSpaceEvenly = false;
+    /* justifyContent - extend list if needed */
+    export let justifyContentSpaceBetween = false;
+    export let justifyContentSpaceAround = false;
+    export let justifyContentSpaceEvenly = false;
 
-  /* alignItems - extend list if needed */
-  export let alignItemsCenter = false;
+    /* alignItems - extend list if needed */
+    export let alignItemsCenter = false;
 
-  /* flexWrap; nowrap is default */
-  export let flexWrapWrap = false;
-  export let flexWrapWrapReverse = false;
+    /* flexWrap; nowrap is default */
+    export let flexWrapWrap = false;
+    export let flexWrapWrapReverse = false;
 
-  /* gap between flex elements */
-  export let gap: string = undefined;
+    /* gap between flex elements */
+    export let gap: string = undefined;
 
-  /* custom style */
-  export let style: string = "";
+    /* custom style */
+    export let style: string = "";
 
-  /* styles - note: the class passed has to be made available on a global level or imported to this file directly */
-  let className = "";
-  export { className as class };
+    /* styles - note: the class passed has to be made available on a global level or imported to this file directly */
+    let className = "";
+    export { className as class };
 </script>
 
 <div
-  class={`flex ${className || ""}`}
-  class:flexDirectionColumn
-  class:flexDirectionResponsiveColumnToRow
-  class:flexDirectionResponsiveColumnToRowReverse
-  class:flexDirectionResponsiveRowToColumn
-  class:flexDirectionResponsiveRowToColumnReverse
-  class:justifyContentSpaceBetween
-  class:justifyContentSpaceEvenly
-  class:justifyContentSpaceAround
-  class:alignItemsCenter
-  class:flexWrapWrap
-  class:flexWrapWrapReverse
-  style={`gap: ${gap || "0rem"}; ${style}`}
+    class={`flex ${className || ""}`}
+    class:flexDirectionColumn
+    class:flexDirectionResponsiveColumnToRow
+    class:flexDirectionResponsiveColumnToRowReverse
+    class:flexDirectionResponsiveRowToColumn
+    class:flexDirectionResponsiveRowToColumnReverse
+    class:justifyContentSpaceBetween
+    class:justifyContentSpaceEvenly
+    class:justifyContentSpaceAround
+    class:alignItemsCenter
+    class:flexWrapWrap
+    class:flexWrapWrapReverse
+    style={`gap: ${gap || "0rem"}; ${style}`}
 >
-  <slot />
+    <slot />
 </div>
 
 <style lang="scss">
-  .flex {
-    display: flex;
-  }
-
-  /* flexDirection classes & responsiveness */
-
-  .flexDirectionColumn {
-    flex-direction: column;
-  }
-
-  @media screen and (min-width: 1100px) {
-    .flexDirectionResponsiveColumnToRow {
-      flex-direction: row;
+    .flex {
+        display: flex;
     }
 
-    .flexDirectionResponsiveColumnToRowReverse {
-      flex-direction: row-reverse;
-    }
-  }
+    /* flexDirection classes & responsiveness */
 
-  @media screen and (max-width: 1100px) {
-    .flexDirectionResponsiveRowToColumn {
-      flex-direction: column;
+    .flexDirectionColumn {
+        flex-direction: column;
     }
 
-    .flexDirectionResponsiveRowToColumnReverse {
-      flex-direction: column-reverse;
+    @media screen and (min-width: 1100px) {
+        .flexDirectionResponsiveColumnToRow {
+            flex-direction: row;
+        }
+
+        .flexDirectionResponsiveColumnToRowReverse {
+            flex-direction: row-reverse;
+        }
     }
-  }
 
-  /* justifyContent classes */
+    @media screen and (max-width: 1100px) {
+        .flexDirectionResponsiveRowToColumn {
+            flex-direction: column;
+        }
 
-  .justifyContentSpaceBetween {
-    justify-content: space-between;
-  }
+        .flexDirectionResponsiveRowToColumnReverse {
+            flex-direction: column-reverse;
+        }
+    }
 
-  .justifyContentSpaceEvenly {
-    justify-content: space-evenly;
-  }
+    /* justifyContent classes */
 
-  .justifyContentSpaceAround {
-    justify-content: space-around;
-  }
+    .justifyContentSpaceBetween {
+        justify-content: space-between;
+    }
 
-  /* alignItems classes */
+    .justifyContentSpaceEvenly {
+        justify-content: space-evenly;
+    }
 
-  .alignItemsCenter {
-    align-items: center;
-  }
+    .justifyContentSpaceAround {
+        justify-content: space-around;
+    }
 
-  /* flexWrap classes */
+    /* alignItems classes */
 
-  .flexWrapWrap {
-    flex-wrap: wrap;
-  }
+    .alignItemsCenter {
+        align-items: center;
+    }
 
-  .flexWrapWrapReverse {
-    flex-wrap: wrap-reverse;
-  }
+    /* flexWrap classes */
+
+    .flexWrapWrap {
+        flex-wrap: wrap;
+    }
+
+    .flexWrapWrapReverse {
+        flex-wrap: wrap-reverse;
+    }
 </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,48 +1,78 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import Navbar from "../lib/Navbar/index.svelte";
-  import Footer from "../lib/Footer/index.svelte";
-  import ContentWrapper from "../lib/util/ContentWrapper/index.svelte";
-  import "../app.css";
+    import { onMount } from "svelte";
+    import Navbar from "../lib/Navbar/index.svelte";
+    import Footer from "../lib/Footer/index.svelte";
+    import "../app.css";
 
     // load floating from json
     onMount(() => {});
 </script>
 
 <svelte:head>
-  <meta name="robots" content="noindex" />
+    <meta name="robots" content="noindex" />
 </svelte:head>
-<ContentWrapper>
-  <Navbar
-    brand={"Kirchmaier & Staudacher"}
-    brandStatements={[
-      "Gebäudemanagement",
-      "Immobilienverwaltung",
-      "Projektentwicklung",
-    ]}
-    navbarItems={[
-      "Home",
-      "Services",
-      "About",
-      "Projects",
-      "Customer",
-      "Contact",
-    ]}
-  />
 
-  <main>
-    <slot />
-  </main>
+<div class="content-wrapper">
+    <Navbar
+        brand={"Kirchmaier & Staudacher"}
+        brandStatements={[
+            "Gebäudemanagement",
+            "Immobilienverwaltung",
+            "Projektentwicklung",
+        ]}
+        navbarItems={[
+            "Home",
+            "Services",
+            "About",
+            "Projects",
+            "Customer",
+            "Contact",
+        ]}
+    />
 
-  <div style="height: 1000px;"></div>
+    <main>
+        <slot />
+    </main>
 
-  <Footer />
-</ContentWrapper>
+    <div style="height: 1000px;" />
+
+    <Footer />
+</div>
 
 <style>
-  html,
-  body {
-    font-family: var(--font-primary);
-    background: var(--bg-color);
-  }
+    html,
+    body {
+        font-family: var(--font-primary);
+        background: var(--bg-color);
+    }
+
+    .content-wrapper {
+        max-width: 1400px;
+        margin: auto;
+        padding: 1.5rem;
+    }
+
+    @media screen and (min-width: 600px) {
+        .content-wrapper {
+            padding: 2rem 2rem;
+        }
+    }
+
+    @media screen and (min-width: 800px) {
+        .content-wrapper {
+            padding: 2rem 2.5rem;
+        }
+    }
+
+    @media screen and (min-width: 1400px) {
+        .content-wrapper {
+            padding: 3rem;
+        }
+    }
+
+    @media screen and (min-width: 1800px) {
+        .content-wrapper {
+            padding: 4rem;
+        }
+    }
 </style>
