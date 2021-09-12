ff3a9c10d5a0dca55a0944a89e80bd2b14ad7b67
Robin Rehbein <me@robinrehbein.de>
2021-09-12 19:44:53 +0200

    landing page wip

--- src/lib/Home/index.svelte
+++ src/lib/Home/index.svelte
@@ -27,7 +27,8 @@
         width: 100vw;
     }
     div.wrapper {
-        position: absolute;
+        // position: absolute;
+        position: fixed;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
@@ -36,15 +37,30 @@
     .statements {
         width: fit-content;
         color: white;
+        // mix-blend-mode: difference;
         // background-color: whitesmoke;
     }
     h1 {
         font-size: 5rem;
+        opacity: 1;
+        // font-weight: normal;
+        // color: var(--primary-color);
     }
     .statements {
+        opacity: 1;
         display: flex;
         flex-direction: row;
         font-size: 2rem;
+        gap: 1rem;
+        // font-weight: bold;
+        // color: var(--primary-color);
+    }
+    .statement::after {
+        content: "-";
+        margin-left: 1rem;
+    }
+    .statement:last-child::after {
+        content: "";
     }
     :global(.bg-image) {
         position: absolute;
