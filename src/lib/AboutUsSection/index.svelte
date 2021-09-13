4de61090fabeb24213a9f9b988244aef13869ca3
Andreas Lemberger <andreas.lemberger@mhp.com>
2021-09-13 20:30:35 +0200

    finished about section

--- src/lib/AboutUsSection/index.svelte
+++ src/lib/AboutUsSection/index.svelte
@@ -33,37 +33,52 @@
     </div>
   </Headline>
 
-  <div class="section-content-wrapper">
-    <div>
-      {#each aboutUsContent.boxes as aboutUsElement, i}
-        <Flex
-          flexDirectionColumn
-          flexDirectionResponsiveColumnToRow={!(i % 2 === 0)}
-          flexDirectionResponsiveColumnToRowReverse={i % 2 === 0}
-          justifyContentSpaceBetween
-          gap="2rem"
-          style="margin-bottom:3rem; cursor:pointer;"
+  <div>
+    {#each aboutUsContent.boxes as aboutUsElement, i}
+      <Flex
+        flexDirectionColumn
+        flexDirectionResponsiveColumnToRow={i % 2 === 0}
+        flexDirectionResponsiveColumnToRowReverse={!(i % 2 === 0)}
+        justifyContentSpaceBetween
+        gap="2rem"
+        style="margin-bottom:3rem; cursor:pointer;"
+      >
+        <div
+          class="box-text about-us-text-box"
+          on:click={() => setContentAndShowModal(aboutUsElement, i)}
         >
-          <img
-            src={`${imgBasePath}${aboutUsElement.imgUrl}`}
-            alt={aboutUsElement.imgUrl}
-            class="img-shadow about-us-image-box"
-            on:click={() => setContentAndShowModal(aboutUsElement, i)}
-          />
-          <div
-            class="box-text about-us-text-box"
-            on:click={() => setContentAndShowModal(aboutUsElement, i)}
-          >
-            <p class="box-headline">{aboutUsElement.boxHeadline}</p>
-            <div class="crop-after-three-lines">
-              {@html aboutUsElement.boxText}
-            </div>
+          <p class="box-headline">{aboutUsElement.boxHeadline}</p>
+          <div class="crop-after-three-lines">
+            {@html aboutUsElement.boxText}
           </div>
-        </Flex>
+        </div>
+        <img
+          src={`${imgBasePath}${aboutUsElement.imgUrl}`}
+          alt={aboutUsElement.imgUrl}
+          class="img-shadow about-us-image-box"
+          on:click={() => setContentAndShowModal(aboutUsElement, i)}
+        />
+      </Flex>
+    {/each}
+  </div>
+</div>
+<div class="about-us-numbers">
+  <div class="content-wrapper">
+    <Flex
+      flexDirectionColumn
+      flexDirectionResponsiveColumnToRow
+      justifyContentSpaceBetween
+      alignItemsCenter
+      gap="3rem"
+    >
+      {#each aboutUsContent.numbers as numberEntry}
+        <div class="about-us-number-card">
+          <div class="about-us-number-card-hl">{numberEntry.boxNumber}</div>
+          <div>{numberEntry.boxMetric}</div>
+        </div>
+        <span class="line-break" />
       {/each}
-    </div>
-
-    <div class="about-us-numbers" />
+    </Flex>
   </div>
 </div>
 
@@ -91,6 +106,52 @@
 {/if}
 
 <style lang="scss">
+  .about-us-numbers {
+    background: var(--primary-color);
+    padding: 4rem 0rem;
+  }
+
+  .about-us-number-card {
+    width: 85%;
+    padding: 1.5rem;
+    border-radius: 1rem;
+    text-align: center;
+    font-size: 1.7rem;
+    line-height: 1.5;
+    transition: all ease-in-out 0.25s;
+    position: relative;
+  }
+
+  .about-us-number-card:hover {
+    background: var(--secondary-color);
+    box-shadow: var(--box-shadow-dark);
+    transform: scale(1.03);
+  }
+
+  .line-break {
+    width: 40%;
+    height: 3px;
+    background-color: var(--text-color);
+    border-radius: 0.5rem;
+  }
+  .line-break:last-child {
+    display: none;
+  }
+
+  @media screen and (min-width: 960px) {
+    .about-us-number-card {
+      width: 20%;
+    }
+
+    .line-break {
+      display: none;
+    }
+  }
+
+  .about-us-number-card-hl {
+    font-weight: bold;
+  }
+
   .content-wrapper {
     max-width: 1400px;
     margin: auto;
