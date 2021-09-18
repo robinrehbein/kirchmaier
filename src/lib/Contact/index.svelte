6984d04fe453a694527455752a66983cf166d5bd
Andi Lemberger <andi.lemberger@gmx.de>
2021-09-18 14:43:51 +0200

    changed icon dir

--- src/lib/Contact/index.svelte
+++ src/lib/Contact/index.svelte
@@ -1,82 +1,78 @@
 <script lang="ts">
-    import Headline from "../util/Headline/index.svelte";
-    import { jsonContent } from "../../stores/stores";
-    import Flex from "../util/Flex/index.svelte";
-    import Wrapper from "../util/Wrapper/index.svelte";
+  import Headline from "../util/Headline/index.svelte";
+  import { jsonContent } from "../../stores/stores";
+  import Flex from "../util/Flex/index.svelte";
+  import Wrapper from "../util/Wrapper/index.svelte";
 
-    const contact = $jsonContent.body.contact;
+  const contact = $jsonContent.body.contact;
 
-    const iconMapper = {
-        phone: "/phone.svg",
-        fax: "/fax.svg",
-        mail: "/mail.svg",
-    };
+  const iconMapper = {
+    phone: "/phone.svg",
+    fax: "/fax.svg",
+    mail: "/mail.svg",
+  };
 </script>
 
 <section id="contact">
-    <Wrapper>
-        <div class="section-wrapper">
-            <Headline>
-                <div slot="headline">
-                    {@html contact.headline}
-                </div>
-                <div slot="sub-headline">
-                    {@html contact.subHeadline}
-                </div>
-            </Headline>
-            <Flex
-                flexDirectionColumn
-                flexDirectionResponsiveColumnToRow
-                justifyContentSpaceBetween
-                gap="3rem"
-            >
-                <div class="contact-data">
-                    {#each contact.contactData as contactDataElement}
-                        <Flex style="margin-bottom: 1rem;">
-                            <img
-                                src={iconMapper[contactDataElement.icon]}
-                                class="contact-data-icon"
-                                alt={contactDataElement.text}
-                            />
-                            {contactDataElement.value}
-                        </Flex>
-                    {/each}
-
-                    <form action={`mailto:${contact.mailAddress}`} method="GET">
-                        <button class="button button-mailto" type="submit"
-                            >{contact.buttonText}</button
-                        >
-                    </form>
-                </div>
-
-                <img
-                    src="/map-mock.png"
-                    class="contact-map img-shadow"
-                    alt="map"
-                />
-            </Flex>
+  <Wrapper>
+    <div class="section-wrapper">
+      <Headline>
+        <div slot="headline">
+          {@html contact.headline}
         </div>
-    </Wrapper>
+        <div slot="sub-headline">
+          {@html contact.subHeadline}
+        </div>
+      </Headline>
+      <Flex
+        flexDirectionColumn
+        flexDirectionResponsiveColumnToRow
+        justifyContentSpaceBetween
+        gap="3rem"
+      >
+        <div class="contact-data">
+          {#each contact.contactData as contactDataElement}
+            <Flex style="margin-bottom: 1rem;">
+              <img
+                src={iconMapper[contactDataElement.icon]}
+                class="contact-data-icon"
+                alt={contactDataElement.text}
+              />
+              {contactDataElement.value}
+            </Flex>
+          {/each}
+
+          <form action={`mailto:${contact.mailAddress}`} method="GET">
+            <button class="button button-mailto" type="submit"
+              >{contact.buttonText}</button
+            >
+          </form>
+        </div>
+
+        <img src="/map-mock.PNG" class="contact-map img-shadow" alt="map" />
+      </Flex>
+    </div>
+  </Wrapper>
 </section>
 
 <style lang="scss">
-    @media screen and (min-width: 960px) {
-        .contact-data {
-            width: 27%; //25%;
-            margin-top: auto;
-        }
-
-        .contact-map {
-            width: 48%; //45%;
-        }
+  @media screen and (min-width: 960px) {
+    .contact-data {
+      width: 27%; //25%;
+      margin-top: auto;
     }
 
-    .contact-data-icon {
-        margin-right: 1.2rem;
+    .contact-map {
+      width: 48%; //45%;
     }
+  }
 
-    .button-mailto {
-        margin-top: 1.5rem;
-        width: 100%;
-    }
+  .contact-data-icon {
+    margin-right: 1.2rem;
+  }
+
+  .button-mailto {
+    margin-top: 1.5rem;
+    width: 100%;
+  }
 </style>
